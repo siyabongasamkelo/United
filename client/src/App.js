9f7781b0c974a32a570b4bba43b8901a4f1d4ea9
siyabonga <siyabongasamkelociam@gmail.com>
2025-11-06 11:07:29 +0200

    created a routing system added theme for style components working on #1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,46 @@
-import Home from "./pages/Home";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Homepage from "./pages/HomePage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "styled-components";
 
 function App() {
+  const theme = {
+    color: {
+      primary: "#F87B1B",
+      secondary: " #11224E",
+      tetiary: "#FFC50F",
+      text: "rgba(0,0,0,0.7)",
+      smallText: "rgba(0, 0, 0, 0.6)",
+    },
+    size: {
+      small: "13px",
+      medium: "17px",
+      large: "22px",
+      extraLarge: "32px",
+    },
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route index element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/*" element={<Homepage />} />
+      </Route>
+    )
+  );
+
   return (
-    <div>
-      <Home></Home>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </>
   );
 }
 
