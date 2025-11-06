697cae6f6787db64a8243c4fcf175e41d3263075
siyabonga <siyabongasamkelociam@gmail.com>
2025-11-06 11:12:31 +0200

    changed plans will no longer be having a background image

--- client/src/pages/Home.styled.js
+++ client/src/pages/Home.styled.js
@@ -1,14 +1,5 @@
 import styled from "styled-components";
 
 export const HomeCover = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  height: 100vh;
-
-  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.1)),
-    url(${(props) => props.backgroundImage});
-  background-size: cover;
-  background-position: center;
+  /* background-color: red; */
 `;
