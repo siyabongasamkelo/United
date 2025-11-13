07b2add59972d33b9c0f43d25350120dc78bad73
siyabonga <siyabongasamkelociam@gmail.com>
2025-11-13 12:39:49 +0200

    making the website responsive to mobile devices

--- client/src/pages/Home.styled.js
+++ client/src/pages/Home.styled.js
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
 export const HomeCover = styled.div`
-  height: 90vh;
+  height: 100vh;
 `;
 
 // this is the hero section where everything on the homepage will be
 export const HeroSection = styled.div`
-  height: 75%;
+  height: 65%;
   margin-top: 5%;
   display: flex;
+  @media only screen and (max-width: 600px) {
+    flex-direction: column;
+    height: 100%;
+  }
 `;
 
 //the hero section is devided into two parts
@@ -16,6 +20,10 @@ export const HeroSection = styled.div`
 export const LeftHero = styled.div`
   height: 100%;
   width: 50%;
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+    height: 50%;
+  }
 `;
 
 //this is the right side
@@ -51,7 +59,34 @@ export const RightHero = styled.div`
     width: 100%;
 
     h1 {
-      font-size: 72px;
+      /* font-size: 72px; */
+      font-size: 5rem;
+      color: ${({ theme }) => theme.color.secondary};
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+    height: 50%;
+    width: 100%;
+    .middle-text {
+      display: none;
+    }
+    .lower-text {
+      height: 50%;
+      margin-top: -25%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: start;
+      h1 {
+        /* font-size: 32px; */
+        font-size: 2.5rem;
+        text-align: center;
+      }
+      button {
+        height: 30%;
+        margin-top: 5%;
+      }
     }
   }
 `;
@@ -99,5 +134,36 @@ export const LeftSidePhotoContainer = styled.div`
       width: 90%;
       margin-left: 5%;
     }
+  }
+
+  @media only screen and (max-width: 600px) {
+    margin-top: 15%;
+    height: 100%;
+    .left-side-photos {
+      .upper-photos {
+      }
+    }
+  }
+`;
+
+//This is the part that contains social media icons the lowest part
+export const SocialMedia = styled.div`
+  height: 5%;
+  width: 100%;
+  margin-top: 7%;
+
+  div {
+    width: 13%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    svg {
+      /* margin-left: 15px; */
+      transform: scale(150%);
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+    display: none;
   }
 `;
