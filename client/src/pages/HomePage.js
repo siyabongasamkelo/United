ca8bbd5ba9dc5fafc09102da56c74e05763f5a83
siyabonga <siyabongasamkelociam@gmail.com>
2025-11-17 06:05:15 +0200

    added aboutus page on homepage so that the users can scroll down to view the whole website

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Home from "./Home";
+import Home from "../components/Home";
+import AboutUs from "../components/AboutUs";
 
 const HomePage = () => {
   return (
     <>
       <Home />
+      <AboutUs />
     </>
   );
 };
