a5bdc0549377e5cbd44227d6335c84166c0a7e73
siyabonga <siyabongasamkelociam@gmail.com>
2025-11-17 09:09:17 +0200

    added shadow box to make it look cool when users hover

--- client/src/components/common/AboutUsCard.styled.js
+++ client/src/components/common/AboutUsCard.styled.js
@@ -1,14 +1,29 @@
 import styled from "styled-components";
 
 export const AboutUsCardContainer = styled.div`
-  height: 55vh;
+  /* height: 55vh; */
   width: 25%;
   margin-top: 5%;
   text-align: center;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 5px 15px rgb(255, 247, 209);
+    border: 1px solid rgba(255, 255, 255, 0.8);
+    box-shadow: 0 0 10px rgba(255, 247, 209, 0.7),
+      0 0 20px rgba(255, 247, 209, 0.5);
+    filter: brightness(1.05);
+    box-shadow: 10px 10px 40px 10px ${(props) => props.theme.color.primary};
+  }
+
+  @media only screen and (max-width: 600px) {
+    width: 80%;
+  }
 `;
 
 export const CardImage = styled.div`
-  height: 60%;
+  /* height: 60%; */
+  height: 40vh;
   img {
     height: 100%;
     width: 100%;
@@ -23,5 +38,11 @@ export const CardText = styled.div`
   }
   p {
     color: rgba(0, 0, 0, 0.7);
+  }
+
+  @media only screen and (max-width: 600px) {
+    p {
+      font-size: 0.8rem;
+    }
   }
 `;
